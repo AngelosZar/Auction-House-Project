b00b41f366a8525a2901dc5cb87141abdec36ae8
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-11-19 20:06:44 +0100

    working on function to grab the data from user and do the api call

--- src/js/controllers/auth/sign_up.js
+++ src/js/controllers/auth/sign_up.js
@@ -1,7 +1,36 @@
 import { signUpApiCall } from '../../model/auth/sign_up';
-export async function onSignUpUser(params) {
+export async function onSignUpUser(event) {
+  //   event.preventDefault();
+  //   make make this another function
+  const form = event.target;
+  const name = form.name.value;
+  const email = form.email.value;
+  const password = form.password.value;
+  const confirmPassword = form.target.confirmPassword.value;
+  //   up to here
+
+  const userData = {
+    name: name,
+    email: email,
+    password: password,
+    confirmPassword,
+    // optional
+    bio: bio || undefined,
+    avatar: profileImg ? { url: profileImg, alt: profileImgAlt || '' } : undefined,
+  };
+  prompt('i grabbed the data', userData);
   try {
-    await signUpApiCall();
-  } catch (error) {}
-  console.log('i am doing all the work');
+    const response = await signUpApiCall(userData);
+    if (!response.ok) {
+      alert(res.errors[0].message);
+      throw new Error(res.errors[0].message);
+      alert('User created successfully');
+      window.location.href = '/auth/login/';
+    }
+  } catch (error) {
+    console.log('i am doing all the work');
+  } finally {
+    prompt('i am resetting the form');
+    form.reset();
+  }
 }
--- src/js/model/auth/sign_up.js
+++ src/js/model/auth/sign_up.js
@@ -3,28 +3,43 @@ import { API_AUTH_REGISTER } from '../../utilities/constants';
 // const testData = {};
 export const signUpApiCall = async (data) => {
   console.log('i am fetching data');
-  //   try {
-  //     const response = await fetch(API_AUTH_REGISTER, {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify(data),
-  //     });
-  //     if (!res.ok) {
-  //       const errorResponse = await res.json();
-  //       let mappedErrors = 'Unexpected error';
-  //       if (errorResponse.errors && errorResponse.errors.length > 0) {
-  //         mappedErrors = errorResponse.errors.map((error) => error.message).join('\n');
-  //       }
-  //       alert(mappedErrors);
-  //       console.log(mappedErrors);
-  //       throw new Error('Failed to register');
-  //     }
-  //     console.log(response.json());
-  //     console.log(response);
-  //     return response.json();
-  //   } catch (error) {
-  //     return error;
-  //   }
+  try {
+    const response = await fetch(API_AUTH_REGISTER, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    if (!res.ok) {
+      const errorResponse = await res.json();
+      let mappedErrors = 'Unexpected error';
+      if (errorResponse.errors && errorResponse.errors.length > 0) {
+        mappedErrors = errorResponse.errors.map((error) => error.message).join('\n');
+      }
+      alert(mappedErrors);
+      console.log(mappedErrors);
+      throw new Error('Failed to register');
+    }
+    console.log(response.json());
+    console.log(response);
+    return response.json();
+  } catch (error) {
+    return error;
+  }
 };
+// {
+//     "name": "my_username", // Required
+//     "email": "first.last@stud.noroff.no", // Required
+//     "password": "UzI1NiIsInR5cCI", // Required
+//     "bio": "This is my profile bio", // Optional
+//     "avatar": {
+//       "url": "https://img.service.com/avatar.jpg", // Optional
+//       "alt": "My avatar alt text" // Optional
+//     },
+//     "banner": {
+//       "url": "https://img.service.com/banner.jpg", // Optional
+//       "alt": "My banner alt text" // Optional
+//     },
+//     "venueManager": true // Optional
+//   }
