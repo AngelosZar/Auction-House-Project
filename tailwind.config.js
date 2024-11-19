bb499e21b334d537f18f2fd91f8b1289d137de27
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-11-19 16:20:42 +0100

    resolved issues after merge conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,43 @@
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/js/**/*.{js,jsx,ts,tsx}',
+    './index.html',
+    './src/style.css',
+  ],
   theme: {
     extend: {
       fontFamily: {
-        primary: ["Inter", "sans-serif"],
-        secondary: ["Work Sans", "sans-serif"],
+        primary: ['Inter', 'sans-serif'],
+        secondary: ['Work Sans', 'sans-serif'],
       },
       colors: {
         body: {
-          dark: "#0B1423",
-          light: "#F5F5EF",
+          dark: '#0B1423',
+          light: '#F5F5EF',
         },
         green: {
-          1: "#8EEB78",
-          2: "#6FB15F",
-          3: "#418B2F",
+          1: '#8EEB78',
+          2: '#6FB15F',
+          3: '#418B2F',
         },
         purple: {
-          light: "#5333F6",
-          dark: "#2F365B",
+          light: '#5333F6',
+          dark: '#2F365B',
         },
         blue: {
-          dark: "#07193C",
+          dark: '#07193C',
         },
         black: {
-          bright: "#09101B",
+          bright: '#09101B',
         },
         text: {
-          white: "#FDFDFD",
-          black: "#000000",
+          white: '#FDFDFD',
+          black: '#000000',
         },
         light: {
-          cards: "#FFFFFF",
-          component: "#F0F0E8",
+          cards: '#FFFFFF',
+          component: '#F0F0E8',
         },
       },
     },
