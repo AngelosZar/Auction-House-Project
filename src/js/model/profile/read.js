de93c335184b47ca67044b4ce2f5a67fe692cb5c
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-11-21 18:53:55 +0100

    save and return user data from single readProfile  function

--- src/js/model/profile/read.js
+++ src/js/model/profile/read.js
@@ -1,3 +1,4 @@
+import { data } from 'autoprefixer';
 import { API_READ_PROFILES } from '../../utilities/constants';
 // import { headers } from '../../utilities/headers';
 import { API_KEY } from '../../utilities/constants';
@@ -28,6 +29,7 @@ export async function readProfiles(limit = 12, page = 1, query = '_wins') {
 
 export async function readProfile(username, query = '') {
   const { accessToken } = JSON.parse(localStorage.getItem('currentUser'));
+
   try {
     const response = await fetch(`${API_READ_PROFILES}/${username}${query}`, {
       method: 'GET',
@@ -37,11 +39,36 @@ export async function readProfile(username, query = '') {
         'X-Noroff-API-Key': API_KEY,
       },
     });
-    const userData = await response.json();
+    // const userData = await response.json();
+    let name, email, credits, listings, wins, avatarUrl, avatarAlt, bannerUrl, bannerAlt, bio;
+
+    ({
+      data: {
+        name,
+        email,
+        credits,
+        _count: { listings, wins },
+        avatar: { url: avatarUrl, alt: avatarAlt },
+        banner: { url: bannerUrl, alt: bannerAlt },
+        bio,
+      },
+    } = await response.json());
+
+    const userData = {
+      data: {
+        name,
+        email,
+        credits,
+        _count: { listings, wins },
+        avatar: { url: avatarUrl, alt: avatarAlt },
+        banner: { url: bannerUrl, alt: bannerAlt },
+        bio,
+      },
+    };
     if (!response.ok) {
-      throw new Error(userData.errors?.[0]?.message || 'Failed to fetch profiles');
+      throw new Error(userData.errors?.[0]?.message || 'Failed to fetch profile');
     }
-    // console.log('userdata', userData);
+    console.log(userData);
     // console.log(Promise.resolve(userData));
     return userData;
   } catch (error) {
