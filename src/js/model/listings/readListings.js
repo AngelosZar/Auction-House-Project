7405f65482c907601abd23d9af38f85d44bc5bb3
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-11-22 21:45:42 +0100

    read a listing with id

--- src/js/model/listings/readListings.js
+++ src/js/model/listings/readListings.js
@@ -1,40 +1,11 @@
-// Query parameters
-// Not all of the properties of a listing are returned by default. You can use the following optional query parameters to include additional properties in the response.
-
-// Prop	Type	Default
-// _seller
-
-// boolean
-// false
-// _bids
-
-// boolean
-// false
-
-// Filtering
-// You can filter for active listings by using the _active query flag.
-
-// You can filter based on an entry in the tags array by using the _tag query flag. You may only filter by one tag at a time.
-
-// Prop	Type	Default
-// _tag
-
-// string
-// -
-// _active
-
-// boolean
-// -
-
 import { API_READ_LISTINGS } from '../../utilities/constants';
-console.log(API_READ_LISTINGS);
 
 export async function testReadListings() {
   //   console.log(API_READ_LISTINGS);
   //   console.log('I am the testReadListings function');
   //   await readListings();
   console.log('I run the readListings function');
-  await readListings();
+  //   await readListings();
 }
 
 export async function readListings() {
@@ -52,3 +23,17 @@ export async function readListings() {
     throw error;
   }
 }
+
+export async function readListing(id) {
+  try {
+    const response = await fetch(`${API_READ_LISTINGS}/${id}?_seller=true&_bids=true`);
+    const singleListing = await response.json();
+    console.log('singleListing');
+    if (!response.ok) {
+      throw new Error(singleListing.errors?.[0]?.message || 'Failed to fetch listing');
+    }
+    return singleListing;
+  } catch (error) {}
+}
+
+await readListing('529a57c6-2c45-4a7d-9ed9-5df7026628c0');
