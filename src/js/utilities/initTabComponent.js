16dcfb7a760a43de1553554bee879d2a1480e001
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-11-25 19:31:10 +0100

    tab component javascript functionality

---
+++ src/js/utilities/initTabComponent.js
@@ -0,0 +1,45 @@
+export const initTabComponent = function () {
+  console.log('working yo');
+  const tabs = document.querySelectorAll('.tab');
+  const tabContents = document.querySelectorAll('.tab-content');
+  console.log(tabs);
+  console.log(tabContents);
+  //
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+      console.log('clicked');
+      console.log('Tab ID:', tab.id);
+      const targetId = tab.id.replace('tab-', '');
+      console.log('Target Id:', targetId);
+      //
+      tabContents.forEach((content) => {
+        content.classList.add('hidden');
+      });
+      //
+      tabs.forEach((tab) => {
+        tab.classList.remove(
+          'font-bold',
+          'text-green-2',
+          'dark:text-blue-400',
+          'border-green-2',
+          'dark:border-blue-400'
+        );
+        tab.classList.add('border-transparent');
+      });
+
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        targetElement.classList.remove('hidden');
+        tab.classList.add(
+          'font-bold',
+          'text-green-2',
+          'dark:text-blue-400',
+          'border-green-2',
+          'dark:border-blue-400'
+        );
+        //   document.getElementById(targetId).classList.remove('hidden')
+        tab.classList.remove('border-transparent');
+      }
+    });
+  });
+};
--- src/js/views/profile/viewProfile.js
+++ src/js/views/profile/viewProfile.js
@@ -1,6 +1,9 @@
 import { readProfileHandler } from '../../controllers/profile/read';
+import { initTabComponent } from '../../utilities/initTabComponent';
+
 const profilePage = async function () {
   console.log('I am in the profile page');
+  initTabComponent();
   // await readProfileHandler();
 };
 
