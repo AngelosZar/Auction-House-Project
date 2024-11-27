773ef197c76604f05ad668e016442b544c8c1579
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-11-27 16:51:19 +0100

    single listing-tab component

--- src/js/controllers/listings/singleListing.js
+++ src/js/controllers/listings/singleListing.js
@@ -1,2 +1,9 @@
-console.log('halo from the other side');
-console.log('i am the controller one more time');
+import { list } from 'postcss';
+import { initTabComponent } from '../../utilities/initTabComponent';
+
+function main() {
+  console.log('halo from the other side');
+  console.log('i am the controller one more time');
+  initTabComponent();
+}
+document.addEventListener('DOMContentLoaded', main());
