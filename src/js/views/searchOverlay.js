c4f711325c2b29d9e6f56aab6a1b3421e1723667
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-11-28 14:26:43 +0100

    search overlay. created and toggle on screen

--- src/js/views/searchOverlay.js
+++ src/js/views/searchOverlay.js
@@ -1,23 +1,14 @@
 export const searchOverlay = async function () {
-  const ParentContainer = document.querySelector('main');
-  const searchOverlay = document.createElement('section');
+  const parentContainer = document.querySelector('main');
   const searchBtn = document.querySelector('#searchBtn');
-  //   console.log(searchBtn);
-  searchOverlay.classList.add(
-    'relative',
-    'bg-light-component',
-    'dark:bg-purple-dark',
-    'border-y-2',
-    'dark:border-none',
-    'flex',
-    'flex-col',
-    'justify-center',
-    'items-center',
-    'h-lvh',
-    'w-full'
-  );
+  let isOverlayOpen = false;
+
   const searchOverlayContent = `
-          <div class="flex justify-center items-center py-16">
+     <section
+        class="relative bg-light-component dark:bg-purple-dark border-y-2 dark:border-none flex flex-col justify-center items-center h-lvh"
+        id="searchContainer"
+      >
+        <div class="flex justify-center items-center py-16">
           <div class="max-w-md">
             <img src="./media/header_logos/grayscale-transparent.png" alt="" srcset="" />
             <h1 class="text-4xl font-extrabold text-center mt-4">
@@ -36,31 +27,20 @@ export const searchOverlay = async function () {
             <button class="btn btn-primary dark:btn-primary-dark mt-4 mb-[50%]">Search</button>
           </form>
         </div>
+      </section>
   `;
 
-  //   console.log('i am a search overlay');
+  //   event listener on searc hbtn
+  searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (!isOverlayOpen) {
+      parentContainer.insertAdjacentHTML('afterbegin', searchOverlayContent);
+    } else {
+      document.querySelector('#searchContainer').remove();
+      // can be clicked and inserted only once ..
+      // pressing again on search btn closes the searchOverlay. toggle visibility // seperate function /event listener to toggle
+      // console.log(e.target);
+    }
+    isOverlayOpen = !isOverlayOpen;
+  });
 };
-// <!-- <section
-//         class="relative bg-light-component dark:bg-purple-dark border-y-2 dark:border-none flex flex-col justify-center items-center h-lvh"
-//         id="searchContainer"
-//       >
-// <div class="flex justify-center items-center py-16">
-//   <div class="max-w-md">
-//     <img src="./media/header_logos/grayscale-transparent.png" alt="" srcset="" />
-//     <h1 class="text-4xl font-extrabold text-center mt-4">
-//       Search for treasures
-//   </div>
-// </div>
-// <div class="w-full max-w-2xl px-8">
-//   <form action="" class="flex flex-col items-center  w-full ">
-//     <input
-//       type="text"
-//       name="search"
-//       id="search"
-//       placeholder="Search for items"
-//       class="input-forms"
-//     />
-//     <button class="btn btn-primary dark:btn-primary-dark mt-4 mb-[50%]">Search</button>
-//   </form>
-// </div>
-//       </section> -->
