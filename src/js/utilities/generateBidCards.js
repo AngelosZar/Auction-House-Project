8bba6798a9cb4dc52a159c60dd67e92973582ee4
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-11-28 20:37:21 +0100

    add event listener on bid btn

--- src/js/utilities/generateBidCards.js
+++ src/js/utilities/generateBidCards.js
@@ -1,3 +1,4 @@
+import { list } from 'postcss';
 import { readListings } from '../model/listings/readListings';
 import { formatDateTime } from '../utilities/formatDateTime';
 
@@ -18,34 +19,29 @@ export const generateHtml = function (listings, parentContainer) {
   // console.log('listings:', listings);
   listings.forEach((listing) => {
     const cards = createSingleBidCard(listing);
-    //   const htmlContent = `<div class="p-6 border bg-light-cards rounded-lg border-gray-400 dark:border-purple-dark dark:bg-blue-dark max-w-md h-full flex flex-col justify-between shadow-md" data-listing-id="${listing.id}" tags-category="${listing.tags?.[0]?.substring(0, 2)}">
-    //   <div class="w-full aspect-[4/3] overflow-hidden pb-2">
-    //     <img
-    //        src="${listing.media[0].url}"
-    //       alt="${listing.media[0].alt}"
-    //       class="w-full h-full object-cover"
-    //     />
-    //   </div>
-    //   <p class="text-md font-semibold py-2">${listing.title}</p>
-    //   <p>${listing.seller.name}</p>
-    //   <div class="flex flex-col">
-    //     <p class="text-xs text-left">Ends at ${formatDateTime(listing.endsAt)}</p>
-    //     <p class="text-xs text-left">Highest current bid : ${listing.bids.length > 0 ? Math.max(...listing.bids.map((bid) => bid.amount)) : 'No bids yet'}</p>
-    //   </div>
-    //   <a href="#" class="btn btn-secondary dark:btn-secondary-dark text-xs self-end mt-2">
-    //     Bid href
-    //   </a>
-    // </div> `;
-    // console.log(listing[0]);
-    // console.log(listing.tags?.slice(0, 2))
-    // console.log(listing.tags?.[0]?.substring(0, 2));
     parentContainer.insertAdjacentHTML('beforeend', cards);
+    // document.querySelector('#bid-for-Listing').addEventListener('click', (e) => {
+    //   e.preventDefault();
+    //   console.log('I am the bid button');
+    //   console.log('clicked');
+    // });
+  });
+  //
+  document.querySelectorAll('[data-bid-button]').forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      e.preventDefault();
+      console.log('I am the bid button');
+      console.log('clicked');
+      console.log(e.target.closest('[data-listing-id]').dataset.listingId);
+      const listingId = e.target.closest('[data-listing-id]').dataset.listingId;
+      return listingId;
+    });
   });
 };
 
 export const createSingleBidCard = function (listing) {
   const card = `
-  <div class="p-6 border bg-light-cards rounded-lg border-gray-400 dark:border-purple-dark dark:bg-blue-dark max-w-md h-full flex flex-col justify-between shadow-md" data-listing-id="${listing.id}" tags-category="${listing.tags?.[0]?.substring(0, 2)}">
+  <div class="p-6 border bg-light-cards rounded-lg border-gray-400 dark:border-purple-dark dark:bg-blue-dark max-w-md h-full flex flex-col justify-between shadow-md" data-listing-id="${listing.id}" data-tags="${listing.tags?.[0]?.substring(0, 2)}">
   <div class="w-full aspect-[4/3] overflow-hidden pb-2">
     <img
        src="${listing.media[0].url}"
@@ -59,8 +55,10 @@ export const createSingleBidCard = function (listing) {
     <p class="text-xs text-left">Ends at ${formatDateTime(listing.endsAt)}</p>
     <p class="text-xs text-left">Highest current bid : ${listing.bids.length > 0 ? Math.max(...listing.bids.map((bid) => bid.amount)) : 'No bids yet'}</p>
   </div>
-  <a href="#" class="btn btn-secondary dark:btn-secondary-dark text-xs self-end mt-2"   id="bid-for-Listing" >
+  <a href="#" class="btn btn-secondary dark:btn-secondary-dark text-xs self-end mt-2"   id="bid-for-Listing" 
+  data-bid-button>
     Bid
   </a>`;
+
   return card;
 };
