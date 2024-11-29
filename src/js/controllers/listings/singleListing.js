53f243b213727920a2ad54d16b183ca6b2ebb432
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-11-29 20:10:32 +0100

    added event listeners to single listing card on hero

--- src/js/controllers/listings/singleListing.js
+++ src/js/controllers/listings/singleListing.js
@@ -4,7 +4,11 @@ import { singleListingContainer } from '../../views/listings/single_listing';
 import { createSingleListingCard } from '../../model/listings/singleListing';
 import { list } from 'postcss';
 //
+import { addEventListenersToSingleListingCard } from '../../model/listings/singleListing';
 import { tabComponentHeader } from '../../model/listings/singleListing';
+import { authGuard } from '../../utilities/authGaurd';
+import { validateBid } from '../../utilities/generateBidCards';
+import { bidOnListing } from '../../model/listings/bid';
 // import { generateHtml } from '../../utilities/generateBidCards';
 
 async function renderHero() {
@@ -18,11 +22,39 @@ async function renderHero() {
     const response = await readListing(listingId);
     // console.log('Raw response:', response);
     const listing = response.data;
-    console.log(listing);
+
+    // console.log(listing);
 
     const card = createSingleListingCard(listing);
     parentContainer.insertAdjacentHTML('afterbegin', card);
     //
+    document.querySelector('[data-bid-button]').addEventListener('click', async (e) => {
+      e.preventDefault();
+      const listingId = e.target.closest('[data-listing-id]').dataset.listingId;
+      const highestBid = e.target.closest('[data-highest-bid]').dataset.highestBid;
+      //
+      if (authGuard()) {
+        const bidAmount = prompt('Enter your bid amount. min bid amount is ' + (+highestBid + 1));
+        const isValidBid = validateBid(bidAmount, highestBid);
+        if (!isValidBid) return;
+        const bid = {
+          amount: Number(bidAmount),
+        };
+        console.log('bid:', bid);
+        await bidOnListing(bid, listingId);
+        window.location.reload();
+      }
+    });
+
+    document.querySelectorAll('img').forEach((img) => {
+      img.addEventListener('click', (e) => {
+        e.preventDefault();
+        const listingId = e.target.closest('[data-listing-id]').dataset.listingId;
+        console.log('listingId:', listingId);
+        localStorage.setItem('listingId', listingId);
+        window.location.href = '/biddings/single-listing/';
+      });
+    });
   } catch (error) {
     console.log(error);
     throw error;
--- src/js/model/listings/singleListing.js
+++ src/js/model/listings/singleListing.js
@@ -2,37 +2,58 @@ import { formatDateTime } from '../../utilities/formatDateTime';
 
 export const createSingleListingCard = function (listing) {
   const listingHtml = `
-          <div class=" data-listing-id="${listing.id}" data-tags="${listing.tags?.[0]?.substring(0, 2)}" 
-           data-highest-bid='${listing?.bids?.length ? Math.max(...listing.bids.map((bid) => bid.amount)) : Number(1)}' ">
-         <div class="aspect-[4/3] overflow-hidden">
-         <img
-         src="${listing?.media[0]?.url}"
-         alt="${listing?.media[0]?.alt}"
-        class="w-full h-auto object-cover rounded-lg"
-         />
-         </div>
-          </div>
-          <div
-            class="bg-light-cards dark:bg-purple-dark py-4 px-4 flex flex-col md:flex-row md:w-max-[50%] rounded-xl shadow-lg w-full h-auto justify-between"
-          >
-            <div class="space-y-2">
-              <h5>${listing?.title}</h5>
-              <p>Seller: ${listing?.seller.name}</p>
-              <p>Description: ${listing?.description}</p>
-              <p>Current Price: ${listing?.bids?.length > 0 ? Math.max(...listing.bids.map((bid) => bid.amount)) : Number(1)}</p>
-              <p>Ends at: ${formatDateTime(listing?.endsAt)}</p>
-            </div>
-            <div class="flex justify-end mt-auto">
-              <a href="#" class="btn btn-secondary dark:btn-secondary-dark self-end">Place bid</a>
-            </div>
-          </div>
+        <div
+  data-listing-id="${listing?.id}"
+  data-highest-bid="${listing?.bids?.length ? Math.max(...listing?.bids?.map((bid) => bid.amount)) : 1}"
+>
+  <div class="aspect-[4/3] overflow-hidden flex justify-center items-center">
+    <img
+      src="${listing?.media?.[0]?.url}"
+      alt="${listing?.media?.[0]?.alt}"
+      class="w-full h-full object-contain rounded-lg"
+    />
+  </div>
+  <div
+    class="bg-light-cards dark:bg-purple-dark py-4 px-4 flex flex-col md:flex-row md:w-max-[50%] rounded-xl shadow-lg w-full h-auto justify-between"
+  >
+    <div class="space-y-2">
+      <h5>${listing?.title}</h5>
+      <p>Seller: ${listing?.seller?.name}</p>
+      <p>Description: ${listing?.description}</p>
+      <p>
+        Current Price: ${
+          listing?.bids?.length > 0 ? Math.max(...listing?.bids?.map((bid) => bid.amount)) : 1
+        }
+      </p>
+      <p>Ends at: ${formatDateTime(listing?.endsAt)}</p>
+    </div>
+    <div class="flex justify-end mt-auto">
+      <a href="#" class="btn btn-secondary dark:btn-secondary-dark self-end" data-bid-button>
+        Place bid
+      </a>
+    </div>
+  </div>
+</div>
       `;
-  console.log('i am working here on generating');
+
+  console.log('data-listing-id:', listing.id);
   return listingHtml;
 };
 // fix indentetion when done
 // fix the styling of the card maybe flex 50/50
 
+export function addEventListenersToSingleListingCard(listingId) {
+  const placeBidBtn = document.querySelector('#bid-for-Listing');
+  placeBidBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('Place bid button clicked');
+    console.log(e.target);
+    // const listingId = e.currentTarget.closest('[data-listing-id]').dataset.listingId;
+    // console.log('listingId:', listingId);
+    // localStorage.setItem('listingId', listingId);
+    // window.location.href = '/biddings/single-listing/';
+  });
+}
 export const tabComponentHeader = function () {
   const html = `
   <div class="px-8 flex justify-center md:justify-start">
