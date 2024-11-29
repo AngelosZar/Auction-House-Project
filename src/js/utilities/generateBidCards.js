b0c5f278311fd05921b9a930ad3c9cef8ba373c1
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-11-29 16:34:51 +0100

    api call to place a bid on an item and reload the page

--- src/js/utilities/generateBidCards.js
+++ src/js/utilities/generateBidCards.js
@@ -1,6 +1,8 @@
 import { readListings } from '../model/listings/readListings';
 import { formatDateTime } from '../utilities/formatDateTime';
 import { authGuard } from '../utilities/authGaurd';
+import { bidOnListing } from '../model/listings/bid';
+import { readListing } from '../model/listings/readListings';
 
 export async function generateBidCards() {
   try {
@@ -15,14 +17,14 @@ export async function generateBidCards() {
   }
 }
 
-export const generateHtml = function (listings, parentContainer) {
+export const generateHtml = async function (listings, parentContainer) {
   listings.forEach((listing) => {
     const cards = createSingleBidCard(listing);
     parentContainer.insertAdjacentHTML('beforeend', cards);
   });
 
   document.querySelectorAll('[data-bid-button]').forEach((btn) => {
-    btn.addEventListener('click', (e) => {
+    btn.addEventListener('click', async (e) => {
       e.preventDefault();
       const listingId = e.target.closest('[data-listing-id]').dataset.listingId;
       const highestBid = e.target.closest('[data-highest-bid]').dataset.highestBid;
@@ -31,9 +33,21 @@ export const generateHtml = function (listings, parentContainer) {
         const bidAmount = prompt('Enter your bid amount. min bid amount is ' + (+highestBid + 1));
         const isValidBid = validateBid(bidAmount, highestBid);
         if (!isValidBid) return;
-        const bid = bidAmount;
+        // const bid = bidAmount;
+        const bid = {
+          amount: Number(bidAmount),
+        };
         console.log('bid:', bid);
-        return listingId, bid;
+        // return listingId, bid;
+        await bidOnListing(bid, listingId);
+        window.location.reload();
+        // await generateBidCards();
+        // console.log('Listing data:', listing);
+        // console.log('Listing in generateHtml:', listing);
+        // cannot refresh the page  getting errors
+        // const updatedListing = await readListing(listingId);
+        // const cardWithBid = document.querySelector(`[data-listing-id="${listingId}"]`);
+        // cardWithBid.innerHTML = createSingleBidCard(updatedListing);
       }
     });
   });
@@ -47,32 +61,31 @@ export const generateHtml = function (listings, parentContainer) {
       // window.location.href = '/biddings/single-listing/';
     });
   });
-  // POST request to the backend to place the bid
 };
 
 export const createSingleBidCard = function (listing) {
   const card = `
   <div class="p-6 border bg-light-cards rounded-lg border-gray-400 dark:border-purple-dark dark:bg-blue-dark max-w-md h-full flex flex-col justify-between shadow-md" 
   data-listing-id="${listing.id}" data-tags="${listing.tags?.[0]?.substring(0, 2)}" 
-  data-highest-bid='${listing.bids.length > 0 ? Math.max(...listing.bids.map((bid) => bid.amount)) : Number(1)}' ">
+  data-highest-bid='${listing?.bids?.length ? Math.max(...listing.bids.map((bid) => bid.amount)) : Number(1)}' ">
   <div class="w-full aspect-[4/3] overflow-hidden pb-2">
     <img
-       src="${listing.media[0].url}"
-      alt="${listing.media[0].alt}"
+       src="${listing?.media[0]?.url}"
+      alt="${listing?.media[0]?.alt}"
       class="w-full h-full object-cover"
     />
   </div>
-  <p class="text-md font-semibold py-2">${listing.title}</p>
-  <p>${listing.seller.name}</p>
+  <p class="text-md font-semibold py-2">${listing?.title}</p>
+  <p>${listing?.seller.name}</p>
   <div class="flex flex-col">
-    <p class="text-xs text-left">Ends at ${formatDateTime(listing.endsAt)}</p>
-    <p class="text-xs text-left">Highest current bid : ${listing.bids.length > 0 ? Math.max(...listing.bids.map((bid) => bid.amount)) : 'No bids yet'}</p>
+    <p class="text-xs text-left">Ends at ${formatDateTime(listing?.endsAt)}</p>
+    <p class="text-xs text-left">Highest current bid : ${listing?.bids?.length > 0 ? Math.max(...listing.bids.map((bid) => bid.amount)) : 'No bids yet'}</p>
   </div>
   <a href="#" class="btn btn-secondary dark:btn-secondary-dark text-xs self-end mt-2"   id="bid-for-Listing" 
   data-bid-button>
     Bid
   </a>`;
-
+  // console.log('Listing in createSingleBidCard:', listing);
   return card;
 };
 
