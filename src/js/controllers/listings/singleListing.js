3a7beb25fa690c42dd1caed6158fd01147826672
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-11-29 18:17:31 +0100

    render hero function successfully

--- src/js/controllers/listings/singleListing.js
+++ src/js/controllers/listings/singleListing.js
@@ -1,10 +1,35 @@
 import { initTabComponent } from '../../utilities/initTabComponent';
-import { renderSingleListing } from '../../model/listings/singleListing';
+import { readListing } from '../../model/listings/readListings';
+import { singleListingContainer } from '../../views/listings/single_listing';
+import { createSingleListingCard } from '../../model/listings/singleListing';
+import { list } from 'postcss';
+// import { generateHtml } from '../../utilities/generateBidCards';
+
+async function renderHero() {
+  const listingId = localStorage.getItem('listingId');
+  const parentContainer = singleListingContainer;
+  if (!parentContainer) {
+    console.error('Container not found');
+    return;
+  }
+  try {
+    const response = await readListing(listingId);
+    // console.log('Raw response:', response);
+    const listing = response.data;
+    console.log(listing);
+
+    const card = createSingleListingCard(listing);
+    parentContainer.insertAdjacentHTML('afterbegin', card);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
 
 async function main() {
   console.log('halo from the other side');
-  console.log('i am the controller one more time');
+  await renderHero();
   initTabComponent();
-  await renderSingleListing();
+  console.log('i am the controller one more time');
 }
 document.addEventListener('DOMContentLoaded', main());
--- src/js/model/listings/singleListing.js
+++ src/js/model/listings/singleListing.js
@@ -1,43 +1,33 @@
-import { readListing } from '../listings/readListings';
-import { singleListingContainer } from '../../views/listings/single_listing';
 import { formatDateTime } from '../../utilities/formatDateTime';
 
-export async function renderSingleListing() {
-  const listingId = localStorage.getItem('listingId');
-  console.log(listingId);
-  try {
-    const response = await readListing(listingId);
-    const listing = response.data;
-    const parentContainer = singleListingContainer;
-    // add containers from view to display listing
-    //  generate html for listing
-  } catch (error) {}
-}
-
-const createSingleListingCard = async function (listing) {
+export const createSingleListingCard = function (listing) {
   const listingHtml = `
-        <div class=" data-listing-id="${listing.id}" data-tags="${listing.tags?.[0]?.substring(0, 2)}" 
-         data-highest-bid='${listing?.bids?.length ? Math.max(...listing.bids.map((bid) => bid.amount)) : Number(1)}' ">
-          <img
-            src="${listing?.media[0]?.url}"
-            alt="${listing?.media[0]?.alt}"
-            class="w-full h-auto object-cover rounded-lg"
-          />
-        </div>
-        <div
-          class="bg-light-cards dark:bg-purple-dark py-4 px-4 flex flex-col md:flex-row md:w-max-[50%] rounded-xl shadow-lg w-full h-auto justify-between"
-        >
-          <div class="space-y-2">
-            <h6>${listing?.title}</h6>
-            <p>Seller :${listing?.seller.name}</p>
-            <p>Description :${listing?.description}</p>
-            <p>Current Price :${listing?.bids?.length > 0 ? Math.max(...listing.bids.map((bid) => bid.amount)) : Number(1)}</p>
-            <p>Ends at :${formatDateTime(listing?.endsAt)}</p>
+          <div class=" data-listing-id="${listing.id}" data-tags="${listing.tags?.[0]?.substring(0, 2)}" 
+           data-highest-bid='${listing?.bids?.length ? Math.max(...listing.bids.map((bid) => bid.amount)) : Number(1)}' ">
+         <div class="aspect-[4/3] overflow-hidden">
+         <img
+         src="${listing?.media[0]?.url}"
+         alt="${listing?.media[0]?.alt}"
+        class="w-full h-auto object-cover rounded-lg"
+         />
+         </div>
           </div>
-          <div class="flex justify-end mt-auto">
-            <a href="#" class="btn btn-secondary dark:btn-secondary-dark self-end">Place bid</a>
+          <div
+            class="bg-light-cards dark:bg-purple-dark py-4 px-4 flex flex-col md:flex-row md:w-max-[50%] rounded-xl shadow-lg w-full h-auto justify-between"
+          >
+            <div class="space-y-2">
+              <h5>${listing?.title}</h5>
+              <p>Seller :${listing?.seller.name}</p>
+              <p>Description :${listing?.description}</p>
+              <p>Current Price : ${listing?.bids?.length > 0 ? Math.max(...listing.bids.map((bid) => bid.amount)) : Number(1)}</p>
+              <p>Ends at : ${formatDateTime(listing?.endsAt)}</p>
+            </div>
+            <div class="flex justify-end mt-auto">
+              <a href="#" class="btn btn-secondary dark:btn-secondary-dark self-end">Place bid</a>
+            </div>
           </div>
-        </div>
-    `;
+      `;
+  console.log('i am working here on generating');
   return listingHtml;
 };
+// fix indentetion when done
