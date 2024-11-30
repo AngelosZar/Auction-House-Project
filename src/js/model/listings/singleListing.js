a7812533d5fb97cbe3da96f05c38b5bc419f0b40
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-11-30 08:46:53 +0100

    adjust styles for single page hero

--- src/js/model/listings/singleListing.js
+++ src/js/model/listings/singleListing.js
@@ -2,39 +2,49 @@ import { formatDateTime } from '../../utilities/formatDateTime';
 
 export const createSingleListingCard = function (listing) {
   const listingHtml = `
-        <div
+<div
   data-listing-id="${listing?.id}"
   data-highest-bid="${listing?.bids?.length ? Math.max(...listing?.bids?.map((bid) => bid.amount)) : 1}"
+  class="grid grid-cols-1 md:grid-cols-2 md:py-8 gap-8 pt-16 pb-20 px-4"
 >
   <div class="aspect-[4/3] overflow-hidden flex justify-center items-center">
     <img
-      src="${listing?.media?.[0]?.url}"
-      alt="${listing?.media?.[0]?.alt}"
-      class="w-full h-full object-contain rounded-lg"
+      src="${listing?.media?.[0]?.url || ''}"
+      alt="${listing?.media?.[0]?.alt || 'Image of the listing'}"
+      class="w-full h-full object-cover rounded-lg"
     />
   </div>
+
   <div
-    class="bg-light-cards dark:bg-purple-dark py-4 px-4 flex flex-col md:flex-row md:w-max-[50%] rounded-xl shadow-lg w-full h-auto justify-between"
+    class="bg-light-cards dark:bg-purple-dark py-4 px-4 flex flex-col md:flex-row rounded-xl shadow-lg w-full h-auto justify-between"
   >
-    <div class="space-y-2">
-      <h5>${listing?.title}</h5>
-      <p>Seller: ${listing?.seller?.name}</p>
-      <p>Description: ${listing?.description}</p>
-      <p>
-        Current Price: ${
-          listing?.bids?.length > 0 ? Math.max(...listing?.bids?.map((bid) => bid.amount)) : 1
-        }
-      </p>
-      <p>Ends at: ${formatDateTime(listing?.endsAt)}</p>
-    </div>
-    <div class="flex justify-end mt-auto">
-      <a href="#" class="btn btn-secondary dark:btn-secondary-dark self-end" data-bid-button>
-        Place bid
-      </a>
+    <div class="space-y-2  flex flex-col">
+      <div class="mb-2">
+        <h5 class="mb-4 semi-bold">${listing?.title || 'Untitled Listing'}</h5>
+        <p class="mb-2" >Seller: ${listing?.seller?.name || 'Unknown'}</p>
+        <p class="mb-2" >Description: ${listing?.description || 'No description provided.'}</p>
+        <p class="mb-2" >
+          Current Price: ${
+            listing?.bids?.length > 0 ? Math.max(...listing?.bids?.map((bid) => bid.amount)) : 1
+          }
+        </p>
+        <p class="mb-2" >Ends at: ${formatDateTime(listing?.endsAt || new Date())}</p>
+      </div>
+
+      <span class="mt-4 justify-end align-bottom">
+        <a
+          href="#"
+          class="btn btn-secondary dark:btn-secondary-dark self-end"
+          data-bid-button
+        >
+          Place bid
+        </a>
+      </span>
     </div>
   </div>
 </div>
-      `;
+
+  `;
 
   console.log('data-listing-id:', listing.id);
   return listingHtml;
