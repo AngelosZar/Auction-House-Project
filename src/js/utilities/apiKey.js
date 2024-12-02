f2185023f84e44109908339703fb54714086a701
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-12-02 17:48:54 +0100

    working on getApiKey

--- src/js/utilities/apiKey.js
+++ src/js/utilities/apiKey.js
@@ -4,14 +4,27 @@ import { API_AUTH_KEY } from './constants';
 // import 'dotenv/config';
 // import * as dotenv from 'dotenv';
 // dotenv.config();
+// const currentUser = localStorage.getItem('currentUser');
+// const userObject = JSON.parse(userCredentialsTest);
+// const userCredentials = {
+//   email: userObject.email,
+//   password: userObject.password,
+// };
 
 export async function getApiKey() {
   try {
     const token = await returnToken();
     //
+    const currentUser = localStorage.getItem('currentUser');
+    if (!currentUser) throw new Error('You are not Logged in\nPlease Log in to get API key');
+    //
+    const userObject = JSON.parse(currentUser);
+    //
+    if (!userObject.email || !userObject.password) throw new Error('No user credentials found');
+    //
     const userCredentials = {
-      email: '    testUser91',
-      password: '  /.,Qwerty123',
+      email: userObject.email,
+      password: userObject.password,
     };
     // userinput from log in function
     const response = await fetch(API_AUTH_KEY, {
@@ -28,7 +41,7 @@ export async function getApiKey() {
     if (response.ok) {
       const { apiKey } = await response.json();
       localStorage.setItem('API_KEY', apiKey);
-      import.meta.env.VITE_API_KEY = apiKey;
+      // import.meta.env.VITE_API_KEY = apiKey;
       //   cannot save to .env file
       return apiKey;
     }
