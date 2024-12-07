bd52e663780804478efe646b9dae587754132a4f
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-12-07 14:22:02 +0100

    dots on carousel .set the index of the slides to determine the number of dots created . and the current index of img to have a different color

--- src/js/model/listings/carousel.js
+++ src/js/model/listings/carousel.js
@@ -2,12 +2,38 @@ export const initCarousel = async function () {
   const carousel = document.querySelector('#carousel-component');
   //   console.log('carousel:', carousel);
   const slides = document.querySelectorAll('[data-carousel="slide"]');
-  //   console.log('slides:', slides);
+  console.log('slides:', slides.length);
   const previousBtn = document.querySelector('#carousel-btn-previous');
   const nextBtn = document.querySelector('#carousel-btn-next');
+  const dotsContainer = document.querySelector('#carousel-dots');
+  console.log(previousBtn, nextBtn, dotsContainer);
   //   slides.style.transform = 'scale(0.5)';
+
+  let currentSlide = 0;
   slides.forEach((slide, index) => {
     slide.style.transform = `translateX(${100 * index}%)`;
     // slide.classList.remove('hidden');
   });
+  //
+  slides.forEach((_, index) => {
+    const dot = document.createElement('button');
+    console.log(dot);
+    dot.classList.add('w-3', 'h-3', 'carousel-dot', 'rounded-full', 'bg-white', 'opacity-50');
+    dotsContainer.appendChild(dot);
+    // dot.addEventListener('click', () => {]); // add event listener to dots to change slide
+  });
+
+  const updateCurrentDot = function () {
+    const dots = document.querySelectorAll('.carousel-dot');
+    dots.forEach((dot, index) => {
+      if (index === currentSlide) {
+        dot.classList.remove('opacity-50', 'bg-white');
+        dot.classList.add('opacity-100', 'bg-green-2', 'dark:bg-blue-400');
+      } else {
+        dot.classList.remove('opacity-100', 'bg-green-2', 'dark:bg-blue-400');
+        dot.classList.add('opacity-50');
+      }
+    });
+  };
+  updateCurrentDot();
 };
