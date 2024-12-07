012e9193bb67e6749b04190270e28fe61cbaebf3
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-12-07 13:53:38 +0100

    added and run cypress for testing

--- src/js/model/listings/carousel.js
+++ src/js/model/listings/carousel.js
@@ -1,7 +1,13 @@
 export const initCarousel = async function () {
-  //   const carousel = document.querySelector('#carousel-component');
+  const carousel = document.querySelector('#carousel-component');
   //   console.log('carousel:', carousel);
-  //   const slides = document.querySelectorAll('[data-carousel="slide"]');
+  const slides = document.querySelectorAll('[data-carousel="slide"]');
   //   console.log('slides:', slides);
-  //   slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+  const previousBtn = document.querySelector('#carousel-btn-previous');
+  const nextBtn = document.querySelector('#carousel-btn-next');
+  //   slides.style.transform = 'scale(0.5)';
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${100 * index}%)`;
+    // slide.classList.remove('hidden');
+  });
 };
