594a5f719c45c8adf7d2c8f7b7ffaccd6bf6cbdd
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-12-08 16:35:54 +0100

    added functionality to click on the dot and move to the index of the img

--- src/js/model/listings/carousel.js
+++ src/js/model/listings/carousel.js
@@ -13,13 +13,20 @@ export const initCarousel = async function () {
     slide.style.transform = `translateX(${100 * index}%)`;
     slide.classList.remove('hidden');
   });
-  //
+
   slides.forEach((_, index) => {
     const dot = document.createElement('button');
     console.log(dot);
     dot.classList.add('w-3', 'h-3', 'carousel-dot', 'rounded-full', 'bg-white', 'opacity-50');
     dotsContainer.appendChild(dot);
-    // dot.addEventListener('click', () => {]); // add event listener to dots to change slide
+
+    dot.addEventListener('click', function () {
+      currentSlide = index;
+      slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+      });
+      updateCurrentDot();
+    });
   });
 
   const updateCurrentDot = function () {
@@ -41,7 +48,7 @@ export const initCarousel = async function () {
     });
     updateCurrentDot();
   });
-  //
+
   previousBtn.addEventListener('click', function () {
     currentSlide = (currentSlide - 1 + slides.length) % slides.length;
     slides.forEach((slide, index) => {
