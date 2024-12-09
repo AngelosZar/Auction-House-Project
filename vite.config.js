07b76c1c23be3f02ae59152e15967c8bd4ba2913
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-12-09 15:53:22 +0100

    added update profile page

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
         signIn: resolve(__dirname, 'auth/sign_in/index.html'),
         signUp: resolve(__dirname, 'auth/sign_up/index.html'),
         profile: resolve(__dirname, 'profile/index.html'),
+        updateProfile: resolve(__dirname, 'profile/update/index.html'),
         singleListing: resolve(__dirname, 'biddings/single-listing/index.html'),
       },
     },
