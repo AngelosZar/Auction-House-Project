a62cdc909b7ff7d9c34e6accbcb71bf9064ec9b4
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-12-10 18:08:33 +0100

    fixed quarey parameter for redirecting from create an article to cprofile create a listing

--- src/js/controllers/profile/read.js
+++ src/js/controllers/profile/read.js
@@ -41,7 +41,7 @@ export async function renderProfilePage() {
 const init = async function () {
   await renderProfilePage();
   const params = new URLSearchParams(window.location.search);
-  if (params.get('action') === 'create') {
+  if (params.get('action') === 'create/') {
     document.querySelector('#tab-create-listing').click();
   }
 };
