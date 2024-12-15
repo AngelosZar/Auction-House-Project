cf9c67cd20910f98e95bf904e8b46e3e4c397bec
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-12-15 15:35:02 +0100

    added functionality that when sidebar is open it will close on click outside

--- src/js/utilities/initHeaderSideBar.js
+++ src/js/utilities/initHeaderSideBar.js
@@ -2,10 +2,16 @@ export const initHeaderSideBar = () => {
   const menuBtn = document.querySelector('.menu-btn');
   const sideBar = document.querySelector('.sidebar');
   if (!menuBtn || !sideBar) return;
-  //
+
   menuBtn.addEventListener('click', (e) => {
     e.preventDefault();
     e.stopPropagation();
     sideBar.classList.toggle('-translate-x-full');
+  });
+  //
+  document.addEventListener('click', (e) => {
+    if (!sideBar.contains(e.target) && !menuBtn.contains(e.target)) {
+      sideBar.classList.add('-translate-x-full');
+    }
   });
 };
