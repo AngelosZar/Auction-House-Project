737e1b7fa29fe0a47282aab8979d609738db361b
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-01-08 16:15:30 +0100

    delete later

--- src/main.js
+++ src/main.js
@@ -4,13 +4,19 @@ import { logOut } from './js/utilities/logout';
 import { updateNavBar, initNavBarListeners } from './js/utilities/updateNavBar';
 import { searchOverlay } from './js/views/searchOverlay';
 import { initHeaderSideBar } from './js/utilities/initHeaderSideBar';
+import { signInGuestController } from './js/utilities/authservice';
 // import { initThemeToggle } from './js/utilities/initThemeToggle';
-window.addEventListener('DOMContentLoaded', () => {
-  router(window.location.pathname);
-  initHeaderSideBar();
-  // initThemeToggle();
-  document.getElementById('logOutBtn')?.addEventListener('click', logOut);
-  searchOverlay();
-  updateNavBar();
-  initNavBarListeners();
+window.addEventListener('DOMContentLoaded', async () => {
+  try {
+    // await signInGuestController();
+    router(window.location.pathname);
+    initHeaderSideBar();
+    // initThemeToggle();
+    document.getElementById('logOutBtn')?.addEventListener('click', logOut);
+    searchOverlay();
+    updateNavBar();
+    initNavBarListeners();
+  } catch (error) {
+    console.error('Error on main.js:', error);
+  }
 });
